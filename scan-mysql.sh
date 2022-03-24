2c1f0d47dd1481f9f1d6f27f6c5e0d671776ffff
EraserandRain <1349291258@qq.com>
2022-03-24 18:19:15 +0800

    perf: modify iplist for scan-mysql.sh

--- scan-mysql.sh
+++ scan-mysql.sh
@@ -2,10 +2,10 @@
 export LC_ALL=C
 ## set input
 eth=""
-txt=""
+txt="./iplist/linux-device.txt"
 
 read -p "Please input eth name (for example 'enp1s0'): " eth
-read -p "Please input ip list path(txt file,for example './iplist/iplist.txt'): " txt
+# read -p "Please input ip list path(txt file,for example './iplist/iplist.txt'): " txt
 sum=$(cat "$txt"|wc -l)
 ## script loop
 for (( i=0;i<"$sum";i++ ))
@@ -14,7 +14,6 @@ do
     ip=${iparr[$i]}
     echo $ip
     ip addr add "$ip" dev "$eth"
-    # sh DB_Linux_MySQL_check.sh "$ip" /server/abchosting/mysql/bin sysroot chinafu1502 3306
     sh DB_Linux_MySQL_check.sh "$ip" /server/abchosting/mysql/bin trunkey trunkey7771502 3306
 done
 service network restart
