c79130db54bfd1a2ed2d8f2599bebaff5185c74c
EraserandRain <1349291258@qq.com>
2022-03-25 14:55:21 +0800

    perf； commit ssl check

--- scan-device.sh
+++ scan-device.sh
@@ -2,10 +2,10 @@
 export LC_ALL=C
 ## set input
 eth=""
-txt=""
+txt="./iplist/device.txt"
 
 read -p "Please input eth name (for example 'enp1s0'): " eth
-read -p "Please input ip list path(txt file,for example './iplist/iplist.txt'): " txt
+# read -p "Please input ip list path(txt file,for example './iplist/iplist.txt'): " txt
 sum=$(cat "$txt"|wc -l)
 ## script loop
 for (( i=0;i<"$sum";i++ ))
